5932a0f8a5e8f911a54c91a5afd8e07cbd246862
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-05-08 10:05:58 +0900

    PR修正

--- 03.bowling/bow.rb
+++ 03.bowling/bow.rb
@@ -13,10 +13,7 @@ scores.each do |s|
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << { first: s[0], second: s[1], bonus: 0 }
-end
+frames = shots.each_slice(2).map { |s| { first: s[0], second: s[1], bonus: 0 } }
 
 # エラー回避：10フレーム目でスペアが出た時11フレーム目の:secondに0を入れる
 frames[10][:second] = 0 if frames[9][:first] + frames[9][:second] == 10 && frames[9][:first] != 10
@@ -25,26 +22,16 @@ frames.each_with_index do |frame, index|
   # 9フレーム目まではボーナス換算
   # それ以降は不要
   break if index >= 9
+  next_frame = frames[index + 1]
 
   if frame[:first] == 10
     # ストライクの場合
-    if frames[index + 1][:first] == 10
-      # ストライク二連続の場合
-      frame[:bonus] = frames[index + 1][:first] + frames[index + 2][:first]
-    else
-      frame[:bonus] = frames[index + 1][:first] + frames[index + 1][:second]
-    end
+    frame[:bonus] = next_frame[:first] + (next_frame[:first] == 10 ? frames[index + 2][:first] : next_frame[:second])
   elsif frame[:first] + frame[:second] == 10
     # スペアの場合
-    frame[:bonus] = frames[index + 1][:first]
+    frame[:bonus] = next_frame[:first]
   end
 end
 
-# frames.each do |frame|
-#   puts frame.inspect
-# end
-
-# binding.irb
-
-total = frames.sum { |frame| frame[:first] + frame[:second] + frame[:bonus] }
+total = frames.sum { |frame| [:first, :second, :bonus].sum { |key| frame[key] } }
 puts total
